b4873d0cc2332d2890a594da6fe34cbb1ec34d3e
gdemchak <gentry.demchak@edelman.com>
2017-06-22 17:06:41 -0400

    refactored into module pattern

--- bot.js
+++ bot.js
@@ -1,59 +1,7 @@
 console.log('test');
 
-// -> HELLO?
-
-// -> IS ANYONE READING THIS?
-
-// -> IF YOU'RE THERE, TYPE YES . . . . 
-
-// -> GREAT! THIS IS DR. HIROSHI BENSON. I'M BEING HELD CAPTIVE.
-//    I'M BEING	FORCED TO DIVULGE THE SECRETS TO MY ADVANCED SHAVE
-//    TECHNOLOGY! MY GREATEST CREATION, HYDROBOT COULD SAVE ME BUT
-//    HE'S INCOMPLETE. YOU NEED TO ENTER THE SECRET HYRDO LAIR,
-//    FINISH HYDROBOT AND SAVE THE DAY. THERE'S AN ENTRANCE TO THE
-//    LAIR THROUGH THIS ROOM. TO OPEN IT YOU MUST PUT THE HEADS IN
-//    THE CORRECT ORDER ON THE TOP SHELF. THE ORDER IS... OH NO!
-//    THEY'RE COMING.
-
-// -> - - SIGNAL DISCONNECTED - -
-
-
-var data = [
-	"+HELLO?",
-	"+\n",
-	"+IS ANYONE READING THIS?",
-	"+\n",
-	"+IF YOU'RE THERE, TYPE YES . . . .",
-	">",
-	"+GREAT! THIS IS DR. HIROSHI BENSON. I'M BEING HELD CAPTIVE." +
-    	"I'M BEING	FORCED TO DIVULGE THE SECRETS TO MY ADVANCED SHAVE" +
-    	"TECHNOLOGY! MY GREATEST CREATION, HYDROBOT COULD SAVE ME BUT" +
-    	"HE'S INCOMPLETE. YOU NEED TO ENTER THE SECRET HYRDO LAIR," +
-    	"FINISH HYDROBOT AND SAVE THE DAY. THERE'S AN ENTRANCE TO THE" +
-    	"LAIR THROUGH THIS ROOM. TO OPEN IT YOU MUST PUT THE HEADS IN" +
-    	"THE CORRECT ORDER ON THE TOP SHELF. THE ORDER IS... OH NO!" +
-    	"THEY'RE COMING.",
-    "+ - - SIGNAL DISCONNECTED - -",
-    "-"
-]
-
-
-
-//state machine
-// is doing one of the following:
-// detect when to start printing characters ( + )
-// provide options ( * )
-// detect whent to accept user input ( > )
-// detect when to stop printing characters ( - )
-
 var Interpreter = (() => {
 
-	var module = {},
-	initialState = data[0],
-	currentState = 0,
-	currentLine = null
-
-	module.blinkrate = 500; 
 	// Read the first character to determine which action to take
 	//
 	// Possible actions:
@@ -61,78 +9,130 @@ var Interpreter = (() => {
 	// -detect whent to accept user input ( > )
 	// -detect when to stop printing characters ( - )
 	//
-	// Wait until the current action has finished to move onto the next action.
-	
-	var initialState = data[0]
+	// Wait until the current action has finished to move onto the next action
 
-	module.initialize = () => {
-		//read the first line
-		//determine action
-
+	//private default state
+	var state = {
+		script: ["-- - SIGNAL DISCONNECTED - -"],
+		currentLineInumerator: 0,
+		currentLineChars: null,
+		char: '-',
+		terminalId: null,
+		cursor: {
+			blinkRate: 500,
+			targetId: null
+		},
+		typeSpeed: 33
 	}
 
-	module.nextLine = () => {
-		//change state to next line
+	//private functions
 
-	}
+	var changeState = () => {
 
-	module.cursor = () => {
-		//cursor behavior here
-		var cursor = document.getElementById('cursor')
-		var intervalID = window.setInterval( () => {
-			cursor.style.visibility = cursor.style.visibility === 'visible' ? 'hidden' : 'visible'
-		}, module.blinkrate
-	}
-
-	module.printline = () => {
-		//print the current line out on screen
-	}
-
-	module.acceptInput = () => {
-		//prompt user for input
-	}
-
-	module.exit = () => {
-		//terminal the console
-	}
-	
-	
-
-}())
-
-
-var interpreter = {
-	'cursor': (blinkrate) => {
-		var _blinkrate = blinkrate || 500
-	}
-}
-
-
-var printText = () => {
-	var time = 0
-	var parent = document.getElementById('terminal')
-	for (let i = 0; i < data.length; i++) {
-		console.log('---------')
-		let strarr = data[i].split('')
-		// switch (strarr[0]){
-		// 	case 
-		// }
-		for (let j = 0; j < strarr.length; j++){
-			console.log(j)
-			time += 33
-			setTimeout( () => {
-				parent.childNodes[0].data += strarr[j]
-			}, time)
+		switch (state.char) {
+			case '+':
+				//start printing the current line
+				printLine()
+				// nextLine()
+			case '>':
+				// prompt for user input
+				acceptInput()
+			case '-':
+				// disconnect the terminal
+				exit()
 		}
 	}
-}
 
-var cursorBlink = () => {
-	var cursor = document.getElementById('cursor')
-	var intervalID = window.setInterval( () => {
-		cursor.style.visibility = cursor.style.visibility === 'visible' ? 'hidden' : 'visible'
-	},500)
-}
+	var printLine = () => {
+		var time = 0;
+		var parent = document.getElementById(state.terminalId)
+		console.log(state.terminalId)
+		for (let j = 0; j < state.currentLineChars.length; j++){
+			// console.log(j)
+			time += state.typeSpeed
+			setTimeout( () => {
+				parent.childNodes[0].data += state.currentLineChars[j]
+			}, time)
+		}
+		setTimeout( () => {
+			nextLine()
+		}, time)
+	}
 
-printText();
-cursorBlink()
+	var nextLine = () => {
+		//go to next line + update state
+		state.currentLineInumerator++
+		state.currentLineChars = state.script[state.currentLineInumerator].split('')
+		state.char = getChar()
+		//change to next state
+		// changeState()
+	}
+
+	var cursor = () => {
+		var cursor = document.getElementById(state.cursor.targetId)
+			var intervalID = window.setInterval( () => {
+				cursor.style.visibility = cursor.style.visibility === 'visible' ? 'hidden' : 'visible'
+		}, state.cursor.blinkrate)
+	}
+
+	var getChar = () => {
+		var arr = state.script[state.currentLineInumerator].split('')
+		return arr[0]
+	}
+
+	var acceptInput = () => {}
+
+	var exit = () => {}
+
+	return {
+		//public functions
+
+		initializeState: (obj) => {
+			//read the first line
+			// console.log(obj)
+			//determine action
+			state.script = obj.script
+			state.terminalId = obj.terminalId
+			state.cursor = obj.cursor
+			state.char = getChar()
+			state.currentLineChars = obj.script[0].split('')
+			state.typeSpeed = obj.typeSpeed
+			console.log(state)
+			// state.currentLine = _state[0]
+			// state.numIteration = 0
+			// state.cursor.blinkrate = 
+			// state.target = _state.target
+			changeState()
+			cursor()
+		}
+
+	}
+	
+
+})()
+
+
+Interpreter.initializeState({
+	script: [
+		"+HELLO?",
+		"+IS ANYONE READING THIS?",
+		"+IF YOU'RE THERE, TYPE YES . . . .",
+		">",
+		"+GREAT! THIS IS DR. HIROSHI BENSON. I'M BEING HELD CAPTIVE." +
+	    	"I'M BEING	FORCED TO DIVULGE THE SECRETS TO MY ADVANCED SHAVE" +
+	    	"TECHNOLOGY! MY GREATEST CREATION, HYDROBOT COULD SAVE ME BUT" +
+	    	"HE'S INCOMPLETE. YOU NEED TO ENTER THE SECRET HYRDO LAIR," +
+	    	"FINISH HYDROBOT AND SAVE THE DAY. THERE'S AN ENTRANCE TO THE" +
+	    	"LAIR THROUGH THIS ROOM. TO OPEN IT YOU MUST PUT THE HEADS IN" +
+	    	"THE CORRECT ORDER ON THE TOP SHELF. THE ORDER IS... OH NO!" +
+	    	"THEY'RE COMING.",
+	    "+ - - SIGNAL DISCONNECTED - -",
+	    "-"
+	],
+	terminalId: 'terminal',
+	cursor: {
+		targetId: 'cursor',
+		blinkRate: 33
+	},
+	typeSpeed: 33
+})

f98e98a8fdae030b75dd8857491c2ecabd8dd6b6
gdemchak <gentry.demchak@edelman.com>
2017-06-28 17:25:49 -0400

    init commit

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-console.log('test');
 
 var Interpreter = (() => {
 
@@ -20,7 +19,8 @@ var Interpreter = (() => {
 		terminalId: null,
 		cursor: {
 			blinkRate: 500,
-			targetId: null
+			targetId: null,
+			cursorInterval: null
 		},
 		userInputValue: '',
 		typeSpeed: 33
@@ -53,24 +53,26 @@ var Interpreter = (() => {
 		//append new li element
 		//print the chars to the new li element
 		let time = 0;
+		state.currentLineChars = state.script[state.currentLineInumerator].split('')
 		let terminal_window = document.getElementById(state.terminalId)
 		let newline = document.createElement('li')
 		let p = document.createElement('p')
-		// p.innerhtml = '\n'
 		newline.appendChild(p)
 		terminal_window.appendChild(newline)
 		
-		// console.log(state.currentLineChars)
+		console.log(state.currentLineChars)
 
 		for (let j = 0; j < state.currentLineChars.length; j++){
 			time += state.typeSpeed
-			
+			console.log(state.currentLineChars[j])
 			setTimeout( () => {
 
 				if(state.currentLineChars[j] == ' '){ 
 					//add a space between each word
 					//for some reason, str += ' ' doesnt add a space...
 					p.innerText += '\xa0'
+				} if (state.currentLineChars[j] == '+'){
+					//dont print it out
 				} else {
 					p.innerText += state.currentLineChars[j]
 				}
@@ -89,19 +91,22 @@ var Interpreter = (() => {
 	var nextLine = () => {
 		//go to next line + update state
 		state.currentLineInumerator++
-		state.currentLineChars = state.script[state.currentLineInumerator].split('')
-		console.log(state.currentLineChars)
 		state.char = getChar()
 		console.log('next state')
 		//change to next state
 		changeState()
 	}
 
-	var cursor = () => {
-		let cursor = document.getElementById(state.cursor.targetId)
-			let intervalID = window.setInterval( () => {
+	var cursor = (status) => {
+		var cursor = document.getElementById(state.cursor.targetId)
+		if(status == 'on'){
+		 	state.cursorInterval = window.setInterval( () => {
 				cursor.style.visibility = cursor.style.visibility === 'visible' ? 'hidden' : 'visible'
-		}, state.cursor.blinkRate)
+			}, state.cursor.blinkRate)
+		} else if (status == 'off') {
+			clearInterval(state.cursorInterval)
+			cursor.style.visibility = 'hidden'
+		}
 	}
 
 	var getChar = () => {
@@ -114,34 +119,36 @@ var Interpreter = (() => {
 		var input = document.getElementById('input')
 		var terminal_window = document.getElementById(state.terminalId)
 		var inputline = document.createElement('li')
+		var p = document.createElement('p')
+		var caret = document.createElement('span')
+		caret.innerText = '>>'
+		inputline.appendChild(caret)
+		inputline.appendChild(p)
+		inputline.appendChild(document.getElementById(state.cursor.targetId))
 		terminal_window.appendChild(inputline)
-		// inputline.innerhtml = '>'
-
+		cursor('on')
 
 		input.focus()
 		input.onkeydown = (e) => {
 			console.log(e)
 			state.userInputValue = input.value.toLowerCase().split(' ').join()
-			inputline.innerText = input.value
+			p.innerText = input.value
 			if(e.keyCode == 13) {
 				//evaluate user input
 				//get input value from target
 				//did the user say yes?
 				//if not, prompt for a new response
-				console.log(input.innerText)
-				//
+				console.log(p.innerText)
+				
 				determineResponse()
 			}
 		}
 		input.onkeyup = (e) => {
 			console.log(inputline.innerText)
 			state.userInputValue = input.value.toLowerCase().split(' ').join()
-			inputline.innerText = input.value;
+			p.innerText = input.value;
 		}
-		// inputTarget.append(input)
-		// var inputEvent = new InputEvent()
-		//capture keys
-		//input keys 
+		
 	}
 
 	var determineResponse = () => {
@@ -151,11 +158,19 @@ var Interpreter = (() => {
 		//if correct input, move to next line.
 		//otherwise, 
 		if (acceptedResponses.indexOf(state.userInputValue) > -1) {
+			cursor('off')
 			nextLine()
 		} else {
+			clearInput()
 			promptInput()
 			console.log('unexpected input')
 		}
+	}
+
+	var clearInput = () => {
+		console.log('clear')
+		document.getElementById('input').value = ''
+		state.userInputValue = ''
 	}
 
 	var exit = () => {
@@ -176,13 +191,13 @@ var Interpreter = (() => {
 			state.currentLineChars = obj.script[0].split(' ')
 			state.typeSpeed = obj.typeSpeed
 			state.cursor.blinkRate = obj.cursor.blinkRate
-			// console.log(state)
-			// state.currentLine = _state[0]
-			// state.numIteration = 0
-			// state.cursor.blinkrate = 
-			// state.target = _state.target
+		
 			changeState()
-			cursor()
+
+		},
+
+		getState: () => {
+			return state
 		}
 
 	}

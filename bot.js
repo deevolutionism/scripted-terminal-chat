3862e7d2c764d8a049423d6478c9f1d042941f92
gdemchak <gentry.demchak@edelman.com>
2017-06-28 12:29:52 -0400

    technically works...

--- bot.js
+++ bot.js
@@ -22,57 +22,86 @@ var Interpreter = (() => {
 			blinkRate: 500,
 			targetId: null
 		},
+		userInputValue: '',
 		typeSpeed: 33
 	}
 
 	//private functions
 
 	var changeState = () => {
+		// determine which action to take 
+		// based on the first char in the current line
 
 		switch (state.char) {
 			case '+':
 				//start printing the current line
 				printLine()
 				// nextLine()
+				break;
 			case '>':
 				// prompt for user input
-				acceptInput()
+				promptInput()
+				break;
 			case '-':
 				// disconnect the terminal
 				exit()
+				break;
 		}
 	}
 
 	var printLine = () => {
-		var time = 0;
-		var parent = document.getElementById(state.terminalId)
-		console.log(state.terminalId)
+		//append new li element
+		//print the chars to the new li element
+		let time = 0;
+		let terminal_window = document.getElementById(state.terminalId)
+		let newline = document.createElement('li')
+		let p = document.createElement('p')
+		// p.innerhtml = '\n'
+		newline.appendChild(p)
+		terminal_window.appendChild(newline)
+		
+		// console.log(state.currentLineChars)
+
 		for (let j = 0; j < state.currentLineChars.length; j++){
-			// console.log(j)
 			time += state.typeSpeed
+			
 			setTimeout( () => {
-				parent.childNodes[0].data += state.currentLineChars[j]
+
+				if(state.currentLineChars[j] == ' '){ 
+					//add a space between each word
+					//for some reason, str += ' ' doesnt add a space...
+					p.innerText += '\xa0'
+				} else {
+					p.innerText += state.currentLineChars[j]
+				}
+
 			}, time)
 		}
+
 		setTimeout( () => {
+			console.log('next line')
+			// newline.childNodes[state.currentLineInumerator].data += '\n'
 			nextLine()
 		}, time)
+
 	}
 
 	var nextLine = () => {
 		//go to next line + update state
 		state.currentLineInumerator++
 		state.currentLineChars = state.script[state.currentLineInumerator].split('')
+		console.log(state.currentLineChars)
 		state.char = getChar()
+		console.log('next state')
 		//change to next state
-		// changeState()
+		changeState()
 	}
 
 	var cursor = () => {
-		var cursor = document.getElementById(state.cursor.targetId)
-			var intervalID = window.setInterval( () => {
+		let cursor = document.getElementById(state.cursor.targetId)
+			let intervalID = window.setInterval( () => {
 				cursor.style.visibility = cursor.style.visibility === 'visible' ? 'hidden' : 'visible'
-		}, state.cursor.blinkrate)
+		}, state.cursor.blinkRate)
 	}
 
 	var getChar = () => {
@@ -80,9 +109,58 @@ var Interpreter = (() => {
 		return arr[0]
 	}
 
-	var acceptInput = () => {}
+	var promptInput = () => {
 
-	var exit = () => {}
+		var input = document.getElementById('input')
+		var terminal_window = document.getElementById(state.terminalId)
+		var inputline = document.createElement('li')
+		terminal_window.appendChild(inputline)
+		// inputline.innerhtml = '>'
+
+
+		input.focus()
+		input.onkeydown = (e) => {
+			console.log(e)
+			state.userInputValue = input.value.toLowerCase().split(' ').join()
+			inputline.innerText = input.value
+			if(e.keyCode == 13) {
+				//evaluate user input
+				//get input value from target
+				//did the user say yes?
+				//if not, prompt for a new response
+				console.log(input.innerText)
+				//
+				determineResponse()
+			}
+		}
+		input.onkeyup = (e) => {
+			console.log(inputline.innerText)
+			state.userInputValue = input.value.toLowerCase().split(' ').join()
+			inputline.innerText = input.value;
+		}
+		// inputTarget.append(input)
+		// var inputEvent = new InputEvent()
+		//capture keys
+		//input keys 
+	}
+
+	var determineResponse = () => {
+		//temporary accept values
+		var acceptedResponses = ['ya','yes','yeah','ok']
+		console.log('response here!')
+		//if correct input, move to next line.
+		//otherwise, 
+		if (acceptedResponses.indexOf(state.userInputValue) > -1) {
+			nextLine()
+		} else {
+			promptInput()
+			console.log('unexpected input')
+		}
+	}
+
+	var exit = () => {
+
+	}
 
 	return {
 		//public functions
@@ -95,9 +173,10 @@ var Interpreter = (() => {
 			state.terminalId = obj.terminalId
 			state.cursor = obj.cursor
 			state.char = getChar()
-			state.currentLineChars = obj.script[0].split('')
+			state.currentLineChars = obj.script[0].split(' ')
 			state.typeSpeed = obj.typeSpeed
-			console.log(state)
+			state.cursor.blinkRate = obj.cursor.blinkRate
+			// console.log(state)
 			// state.currentLine = _state[0]
 			// state.numIteration = 0
 			// state.cursor.blinkrate = 
@@ -118,21 +197,24 @@ Interpreter.initializeState({
 		"+IS ANYONE READING THIS?",
 		"+IF YOU'RE THERE, TYPE YES . . . .",
 		">",
-		"+GREAT! THIS IS DR. HIROSHI BENSON. I'M BEING HELD CAPTIVE." +
-	    	"I'M BEING	FORCED TO DIVULGE THE SECRETS TO MY ADVANCED SHAVE" +
-	    	"TECHNOLOGY! MY GREATEST CREATION, HYDROBOT COULD SAVE ME BUT" +
-	    	"HE'S INCOMPLETE. YOU NEED TO ENTER THE SECRET HYRDO LAIR," +
-	    	"FINISH HYDROBOT AND SAVE THE DAY. THERE'S AN ENTRANCE TO THE" +
-	    	"LAIR THROUGH THIS ROOM. TO OPEN IT YOU MUST PUT THE HEADS IN" +
-	    	"THE CORRECT ORDER ON THE TOP SHELF. THE ORDER IS... OH NO!" +
-	    	"THEY'RE COMING.",
-	    "+ - - SIGNAL DISCONNECTED - -",
+		"+GREAT! THIS IS DR. HIROSHI BENSON.", 
+		"+I'M BEING HELD CAPTIVE.",
+	    "+I'M BEING	FORCED TO DIVULGE THE SECRETS TO MY ADVANCED SHAVE TECHNOLOGY!",
+	    "+MY GREATEST CREATION, HYDROBOT COULD SAVE ME BUT HE'S INCOMPLETE.",
+	    "+YOU NEED TO ENTER THE SECRET HYRDO LAIR,",
+	    "+FINISH HYDROBOT AND SAVE THE DAY.", 
+	    "+THERE'S AN ENTRANCE TO THE LAIR THROUGH THIS ROOM.",
+	    "+TO OPEN IT YOU MUST PUT THE HEADS IN THE CORRECT ORDER ON THE TOP SHELF.",
+	    "+THE ORDER IS... OH NO!",
+	   	"+THEY'RE COMING.",
+	    "+- - SIGNAL DISCONNECTED - -",
 	    "-"
 	],
 	terminalId: 'terminal',
+	inputTarget: 'input',
 	cursor: {
 		targetId: 'cursor',
-		blinkRate: 33
+		blinkRate: 500
 	},
 	typeSpeed: 33
 })
